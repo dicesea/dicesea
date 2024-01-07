8eed59c9f083bf9666701c5fbf4c250dd4771e15
kingsleynwoye <kingsleynwoye001@gmail.com>
2024-01-07 14:30:59 +0100

    Update asset and profile page

--- client/src/pages/asset/[slug].tsx
+++ client/src/pages/asset/[slug].tsx
@@ -248,24 +248,22 @@ export default function Slug() {
               </Htssvatv>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                width={20}
-                height={20}
+                fill="currentColor"
+                width={40}
+                height={40}
                 style={{
-                  backgroundColor: "#eee",
+                  backgroundColor: "#fff",
                   borderRadius: "50px",
-                  padding: "10px",
                   cursor: "pointer",
+                  boxShadow: "0 10px 12px rgba(0,0,0,0.07)",
                 }}
                 onClick={() => router.back()}
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  fill-rule="evenodd"
+                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm4.28 10.28a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 1 0-1.06 1.06l1.72 1.72H8.25a.75.75 0 0 0 0 1.5h5.69l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3Z"
+                  clip-rule="evenodd"
                 />
               </svg>
             </div>
